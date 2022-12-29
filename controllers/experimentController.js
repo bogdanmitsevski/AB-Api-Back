5665df107c09608a4fabeda48f9821cd65a97383
Bohdan Mitsevskyi <mitsevskyi.corporate@gmail.com>
2022-12-29 15:54:27 +0200

    Describe experiments controller

--- controllers/experimentController.js
+++ controllers/experimentController.js
@@ -1,7 +1,31 @@
+const { Experiments } = require('../models');
+const { Devices } = require('../models');
 class DataController {
     async getData(req, res) {
         try {
-            res.json('Hello, getData router');
+            const allExperiments = await Experiments.findAll();
+            const totalDevicesByGroupA = await Devices.count({
+                where: {
+                    experimentId: 1,
+                    newdevice: true
+                }
+            })
+            const totalDevicesByGroupB = await Devices.count({
+                where: {
+                    experimentId: 2,
+                    newdevice: true
+                }
+            })
+            const totalDevicesByGroupC = await Devices.count({
+                where: {
+                    experimentId: 3,
+                    newdevice: true
+                }
+            })
+            const totalNewDevices = await Devices.count({
+                where: { newdevice: true }
+            })
+            res.json({ totalDevicesByGroupA, totalDevicesByGroupB, totalDevicesByGroupC, allExperiments, totalNewDevices });
         }
         catch (e) {
             console.log(e);
